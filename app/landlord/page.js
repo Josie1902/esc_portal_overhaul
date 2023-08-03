4cdb261c286c6fe669d3db45cc1075812fb57546
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-08-04 02:09:42 +0800

    placeholder landing pages for both tenant and landlord

--- app/landlord/page.js
+++ app/landlord/page.js
@@ -1,12 +1,7 @@
-import ListUnits from "@/components/landlord/manageunits/listunits"
-import AddUnitButton from "@/components/landlord/manageunits/addunitbutton"
-
-export default function ManageUnits() {
+export default function LandlordLanding() {
     return (
         <section className="h-full flex flex-col">
-            <h1 className="mb-1 text-4xl font-extrabold">Manage Units</h1>
-            <AddUnitButton></AddUnitButton>
-            <ListUnits></ListUnits>
+            <h1 className="mb-1 text-4xl font-extrabold">Hello Landlords!</h1>
         </section>
     )
 }
--- app/tenant/page.js
+++ app/tenant/page.js
@@ -1,10 +1,7 @@
-import Options from "@/components/tenant/options"
-
-export default function ServiceOptions() {
+export default function TenantLanding() {
     return (
         <section className="h-full flex flex-col justify-center items-center ">
-            <h1 className="mb-1 text-4xl font-extrabold text-center">What can we help you with?</h1>
-            <Options></Options>
+            <h1 className="mb-1 text-4xl font-extrabold text-center">Hello Tenants!</h1>
         </section>
     )
 }
