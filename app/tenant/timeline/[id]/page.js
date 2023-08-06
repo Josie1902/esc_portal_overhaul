fb543010e6d34fcf01bf5365718fa60e22a39c49
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-08-06 14:45:23 +0800

    able to get svcid

--- app/tenant/timeline/[id]/page.js
+++ app/tenant/timeline/[id]/page.js
@@ -1,10 +1,10 @@
-import StepperView from "@/components/landlord/timeline/timeline"
+import StepperView from "@/components/tenant/timeline/timeline"
 
 export default function Timeline({params}) {
     return (
         <section>
             <h1 className="mb-1 text-3xl font-extrabold text-center">ServiceRequestID: {params.id}</h1>
-            <StepperView></StepperView>
+            <StepperView svcid={params.id}></StepperView>
         </section>
     )
 }
